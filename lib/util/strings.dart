515bcdce3623272b0d0706038e516bed4222d4a1
Marco Vazquez <marcovazquez77@hotmail.com>
2018-11-02 14:19:38 -0600

    String class added to have control in the views

---
+++ lib/util/strings.dart
@@ -0,0 +1,6 @@
+class Strings{
+  static const String APP_NAME = "Easy theraphy";
+  static const String WELCOME = "Si eres hipocondriaco, llegaste al lugar correcto, esta aplicación te ayudará a detectar signos tempranos de enfermedades mentales.";
+  static const String QUESTIONS_VIEW = "Preguntas comunes";
+  static const String QUESTIONS_INFO = "Esta sección nos sirve como referente de tus antecedentes, ya viene la parte divertida no te preocupes! :D";
+}
