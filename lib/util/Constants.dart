1971f374728d0433b877ace32621a5523533524e
Marco Vazquez <marcovazquez77@hotmail.com>
2018-11-19 17:59:37 -0600

    Constants used in the whole project and navigation class created

---
+++ lib/util/Constants.dart
@@ -0,0 +1,7 @@
+import 'package:mental_illness_detector/models/IllnessesMembership.dart';
+
+class Constants{
+  static IllnessesMembership firstSection = IllnessesMembership(0,0,0);
+  static IllnessesMembership secondSection = IllnessesMembership(0,0,0);
+  static IllnessesMembership thirdSection = IllnessesMembership(0,0,0);
+}
---
+++ lib/util/navigation.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mental_illness_detector/ui/firstSection.dart';
+import 'package:mental_illness_detector/ui/genericView.dart';
+import 'package:mental_illness_detector/ui/resultSection.dart';
+import 'package:mental_illness_detector/ui/secondSection.dart';
+import 'package:mental_illness_detector/ui/thirdSection.dart';
+
+class Navigation{
+  static void navigateToFirst(BuildContext context) =>
+      Navigator.push(context, MaterialPageRoute(builder: (_) => FirstSection()));
+
+  static void navigateToSecond(BuildContext context) =>
+      Navigator.push(context, MaterialPageRoute(builder: (_) => SecondSection()));
+
+  static void navigateToThird(BuildContext context) =>
+      Navigator.push(context, MaterialPageRoute(builder: (_) => ThirdSection()));
+
+  static void navigateToResult(BuildContext context) =>
+      Navigator.push(context, MaterialPageRoute(builder: (_) => ResultSection()));
+
+  static void navigateToGeneric(BuildContext context) =>
+      Navigator.push(context, MaterialPageRoute(builder: (_) => GenericView()));
+
+  static void popNavigation(context) => Navigator.of(context).pop(true);
+}
