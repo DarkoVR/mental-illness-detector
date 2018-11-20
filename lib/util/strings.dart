de5c03a3cb7fa5c4226559d3066945f1110ba4fb
Marco Vazquez <marcovazquez77@hotmail.com>
2018-11-19 18:06:01 -0600

    Strings file added to use it into the questions

--- lib/util/strings.dart
+++ lib/util/strings.dart
@@ -1,6 +1,36 @@
 class Strings{
+  static const String START = "Iniciar";
   static const String APP_NAME = "Easy theraphy";
   static const String WELCOME = "Si eres hipocondriaco, llegaste al lugar correcto, esta aplicación te ayudará a detectar signos tempranos de enfermedades mentales.";
-  static const String QUESTIONS_VIEW = "Preguntas comunes";
+  static const String FIRST_SECTION = "¿Me ha pasado esto?";
+  static const String SECOND_SECTION = "¿Y que hay de esto?";
+  static const String THIRD_SECTION = "¿Qué ves aquí?";
+  static const String RESULTS = "Resultados";
   static const String QUESTIONS_INFO = "Esta sección nos sirve como referente de tus antecedentes, ya viene la parte divertida no te preocupes! :D";
+  //questions
+  static const String QUESTION_1 = "¿Me cuesta mucho relajarme?";
+  static const String QUESTION_2 = "¿Me di cuenta que tenia la boca seca?";
+  static const String QUESTION_3 = "¿No podía sentir ningún sentimiento positivo?";
+  static const String QUESTION_4 = "¿Se me hizo difícil respirar?";
+  static const String QUESTION_5 = "¿Se me hizo difícil tomar la iniciativa para hacer cosas?";
+  static const String QUESTION_6 = "¿Reaccioné exageradamente en ciertas situaciones?";
+  static const String QUESTION_7 = "¿Sentí que mis manos temblaban?";
+  static const String QUESTION_8 = "¿Sentí que tenia muchos nervios?";
+  static const String QUESTION_9 = "¿Estaba preocupado por situaciones en las cuales podía tener pánico o en las que podría hacer el ridículo?";
+  static const String QUESTION_10 = "¿Sentí que no tenia nada por que vivir?";
+  static const String QUESTION_11 = "¿Noté que me agitaba?";
+  static const String QUESTION_12 = "¿Me sentí triste y deprimido?";
+  static const String QUESTION_13 = "¿No toleré nada que no me permitiera continuar con lo que estaba haciendo?";
+  static const String QUESTION_14 = "¿Sentí que estaba al punto de pánico?";
+  static const String PHOTO_QUESTION_1 = "¿Son animales?";
+  static const String PHOTO_QUESTION_2 = "¿Parece una batalla?";
+  static const String PHOTO_QUESTION_3 = "¿Parece algo malo?";
+  static const String PHOTO_QUESTION_4 = "¿Un par de botas?";
+  //answers
+  static const String NO = "Nada";
+  static const String MORE_OR_LESS = "Un poco";
+  static const String YES = "Bastante";
+  static const String PHOTO_NO = "Para nada";
+  static const String PHOTO_MORE_OR_LESS = "Creo que si";
+  static const String PHOTO_YES = "Si, seguro";
 }
